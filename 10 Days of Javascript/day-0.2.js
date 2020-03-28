370a7145ff7e372e183f2d181bfeb16e239315a8
Brenda Matias <brenda.matias@mesa.com.br>
2020-03-28 16:42:49 -0300

    10 days of javascript - day 1 arithmetic operators

--- 10 Days of Javascript/day-0.2.js
+++ 10 Days of Javascript/day-0.2.js
@@ -1,11 +1,13 @@
 /**
+ *   Day 0: Data Types
+ *
  *   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
  *   Print three lines:
  *   1. The sum of 'firstInteger' and the Number representation of 'secondInteger'.
  *   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
  *   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
  *
- *	Parameter(s):
+ *	 Parameter(s):
  *   secondInteger - The string representation of an integer.
  *   secondDecimal - The string representation of a floating-point number.
  *   secondString - A string consisting of one or more space-separated words.
